a319a4937e5d739e49ffb4484c6dcf737ae5db9d
mr-vinay02 <vinayvini4630@gmail.com>
2025-10-05 17:59:45 +0530

    This will work with DML Commands

--- librarymanagementsystem/DML.sql
+++ librarymanagementsystem/DML.sql
@@ -1,13 +1,18 @@
 -- DML Commands
 
+-- Insert new data into the books table
+insert into books (title , author_id , publication_year , genre , isbn ,available_copies , publisher )
+values
+('Java', 3, 1997, 'Oracle', '9780061122415', 1, 'vinzzz');
 
--- 1. add column publisher to the table books
-alter table books add column publisher varchar(100);
 
+--  Update a member's contact number
+update members set  contact_number = '9901224630' where member_id = 2 ;
 
--- 2. Rename the position column in the staff table to job_title
-alter table staff change  position  job_title varchar(50) ;
+-- Delete a specific loan record
+delete from loans where loan_id = 4;
 
--- 3.drop the email column from the members table:
-alter table membersmembersmembers
-drop column email ;
+-- Insert a new loan record
+insert into loans ( loan_id , book_id , member_id , loan_date , return_date ,actual_return_date )
+values
+(6, 3, 2, '2023-10-05', '2023-10-19', NULL);
